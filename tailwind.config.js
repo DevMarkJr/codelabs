2550854b7f721144ae6cd566bebdbd9785f7e497
DevMarkJr <devromeromark@gmail.com>
2025-03-08 16:41:10 -0500

    init 🔴

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: [    
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
